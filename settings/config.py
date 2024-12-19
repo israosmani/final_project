2266efb0edf21232acb831d2b7bb4ef07a1a642c
root <root@Isra>
2024-12-18 20:53:33 -0500

    modified config in settings

--- settings/config.py
+++ settings/config.py
@@ -1,10 +1,11 @@
 from builtins import bool, int, str
 from pathlib import Path
-from pydantic import  Field, AnyUrl, DirectoryPath
+from pydantic import Field, AnyUrl
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    max_login_attempts: int = Field(default=3, description="Background color of QR codes")
+    max_login_attempts: int = Field(default=3, description="Maximum allowed login attempts before locking")
+    
     # Server configuration
     server_base_url: AnyUrl = Field(default='http://localhost', description="Base URL of the server")
     server_download_folder: str = Field(default='downloads', description="Folder for storing downloaded files")
@@ -15,35 +16,40 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int = Field(default=30, description="Expiration time for access tokens in minutes")
     admin_user: str = Field(default='admin', description="Default admin username")
     admin_password: str = Field(default='secret', description="Default admin password")
-    debug: bool = Field(default=False, description="Debug mode outputs errors and sqlalchemy queries")
     jwt_secret_key: str = "a_very_secret_key"
     jwt_algorithm: str = "HS256"
-    access_token_expire_minutes: int = 15  # 15 minutes for access token
-    refresh_token_expire_minutes: int = 1440  # 24 hours for refresh token
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_minutes: int = 1440
+    profile_upgrade_token_expire_hours: int = Field(default=24, description="Expiration time for profile upgrade tokens in hours")
+
     # Database configuration
     database_url: str = Field(default='postgresql+asyncpg://user:password@postgres/myappdb', description="URL for connecting to the database")
-
-    # Optional: If preferring to construct the SQLAlchemy database URL from components
+    test_database_url: str = Field(default='sqlite+aiosqlite:///test.db', description="URL for the test database")
+    
+    # Optional: Components for constructing database URL
     postgres_user: str = Field(default='user', description="PostgreSQL username")
     postgres_password: str = Field(default='password', description="PostgreSQL password")
     postgres_server: str = Field(default='localhost', description="PostgreSQL server address")
     postgres_port: str = Field(default='5432', description="PostgreSQL port")
     postgres_db: str = Field(default='myappdb', description="PostgreSQL database name")
+
     # Discord configuration
     discord_bot_token: str = Field(default='NONE', description="Discord bot token")
     discord_channel_id: int = Field(default=1234567890, description="Default Discord channel ID for the bot to interact", example=1234567890)
-    #Open AI Key 
-    openai_api_key: str = Field(default='NONE', description="Open AI Api Key")
-    send_real_mail: bool = Field(default=False, description="use mock")
+
+    # OpenAI API Key
+    openai_api_key: str = Field(default='NONE', description="OpenAI API Key")
+
     # Email settings for Mailtrap
     smtp_server: str = Field(default='smtp.mailtrap.io', description="SMTP server for sending emails")
     smtp_port: int = Field(default=2525, description="SMTP port for sending emails")
     smtp_username: str = Field(default='your-mailtrap-username', description="Username for SMTP server")
     smtp_password: str = Field(default='your-mailtrap-password', description="Password for SMTP server")
 
+    send_real_mail: bool = Field(default=False, description="Use mock emails if set to False")
+    debug: bool = Field(default=False, description="Debug mode outputs errors and SQLAlchemy queries")
 
     class Config:
-        # If your .env file is not in the root directory, adjust the path accordingly.
         env_file = ".env"
         env_file_encoding = 'utf-8'
 
